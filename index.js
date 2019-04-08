1a98d308a9df5cb6231610d83adad887d8dac798
Ben Watson <ben.watson@digital.hmrc.gov.uk>
2019-04-08 10:57:27 +0100

    Start of user flow work

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const renderPageIndex = require('./functions/renderPageIndex')
+const renderUserFlow = require('./functions/renderUserFlow')
+const getUserFlow = require('./functions/user-flow')
 
 module.exports.renderPageIndex = renderPageIndex
+module.exports.getUserFlow = getUserFlow
+module.exports.renderUserFlow = renderUserFlow
