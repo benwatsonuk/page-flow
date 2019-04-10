5386f095a67b2c39343e8c190fb24deeef09b067
Ben Watson <ben.watson@digital.hmrc.gov.uk>
2019-04-10 11:09:57 +0100

    Added journeyId and basic pageNav added to individual page template

--- functions/renderUserFlowPage.js
+++ functions/renderUserFlowPage.js
@@ -2,8 +2,8 @@ const common = require('./common')
 const nunjucks = require('nunjucks')
 const path = require('path')
 
-module.exports = function(userFlow, pageFlow, page, stage, version) {
-    const theUserFlow = common.getPageInfoForUserFlow(userFlow, pageFlow, page, stage, version)
+module.exports = function(userFlow, pageFlow, page, stage, version, journeyId) {
+    const theUserFlow = common.getPageInfoForUserFlow(userFlow, pageFlow, page, stage, version, journeyId)
     const env = new nunjucks.Environment(new nunjucks.FileSystemLoader(path.join(__dirname, '../templates/')))
     const output = env.render('userFlowIndividualPage.njk', { userFlow: theUserFlow })
     return output
