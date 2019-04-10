2f5345269c01b4dcbaa58b3d950bf349859c8701
Ben Watson <ben.watson@digital.hmrc.gov.uk>
2019-04-10 14:51:00 +0100

    Added getUserFlow and fixed link reference in user-flow

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const renderPageIndex = require('./functions/renderPageIndex')
 const renderUserFlow = require('./functions/renderUserFlow')
 const renderUserFlowPage = require('./functions/renderUserFlowPage')
-const getUserFlow = require('./functions/user-flow')
 const common = require('./functions/common')
 
 module.exports.renderPageIndex = renderPageIndex
-module.exports.getUserFlow = getUserFlow
+module.exports.getUserFlow = common.pageFlowFromUserFlow
 module.exports.getUserFlowPage = common.getPageInfoForUserFlow
 module.exports.renderUserFlow = renderUserFlow
 module.exports.renderUserFlowPage = renderUserFlowPage
